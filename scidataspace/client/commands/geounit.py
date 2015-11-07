548d6325a042eef8162dfcec8cc0e2307def95f8
Cristian Vlaescu <cvlescu@gmail.com>
2015-11-07 02:26:08 +0200

    now, user cannot add 2 geounits with same name in the same catalog

--- scidataspace/client/commands/geounit.py
+++ scidataspace/client/commands/geounit.py
@@ -11,12 +11,19 @@ def parse_cmd_geounit(cmd_splitted, catalog_id, geounit_id, datasetClient):
         if geounit_name != UNDEFINED:
             geounit_name = geounit_name
 
-            r, data = datasetClient.create_dataset(catalog_id,dict(name=geounit_name))
-            geounit_id = data['id']
+            r, datasets = datasetClient.get_datasets(catalog_id)
+            filtered_datasets = [x for x in datasets if x['name']==geounit_name]
+            if len(filtered_datasets)<1:
+                r, data = datasetClient.create_dataset(catalog_id,dict(name=geounit_name))
+                geounit_id = data['id']
+            elif len(filtered_datasets)== 1:
+                geounit_id = filtered_datasets[0]['id']
+            else:
+                return UNDEFINED, None, "please choose another name; this one seems corrupted"
 
             return geounit_name, geounit_id, ""
         else:
-            return None, None, "cannot understand geounit name"
+            return UNDEFINED, None, "cannot understand geounit name"
 
     elif cmd_2 == "delete":
         pass
@@ -25,7 +32,7 @@ def parse_cmd_geounit(cmd_splitted, catalog_id, geounit_id, datasetClient):
 
     else:
         # geounit something
-        return None, None, "usage: geounit [start <gounit name>|delete]"
+        return UNDEFINED, None, "usage: geounit [start <gounit name>|delete]"
 
-    return None, None, ""
+    return UNDEFINED, None, ""
 
